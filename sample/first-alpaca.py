f22afc97d10e0474d1a5ab6a2e81d5e23251f1c8
dalecb13 <dalecb13@gmail.com>
2025-07-29 15:58:52 -0500

    added account endpoint

--- sample/first-alpaca.py
+++ sample/first-alpaca.py
@@ -1,13 +1,17 @@
 import os
 from dotenv import load_dotenv
 from alpaca.trading.client import TradingClient
+from fastapi import FastAPI
 
 load_dotenv()
 ALPACA_KEY_ID = os.getenv("APCA_API_KEY_ID")
 ALPACA_SECRET_KEY = os.getenv("APCA_API_SECRET_KEY")
 ALPACA_ENDPOINT = os.getenv("APCA_API_ENDPOINT")
 
-trading_client = TradingClient(ALPACA_KEY_ID, ALPACA_SECRET_KEY, paper=True)
-account = trading_client.get_account()
+app = FastAPI()
 
-print(account)
+@app.get("/accounts")
+def read_accounts():
+    trading_client = TradingClient(ALPACA_KEY_ID, ALPACA_SECRET_KEY, paper=True)
+    account = trading_client.get_account()
+    return account
