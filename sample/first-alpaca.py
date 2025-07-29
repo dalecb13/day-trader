326d96fcbba365c60169a2a44b1af31b205b4924
dalecb13 <dalecb13@gmail.com>
2025-07-29 16:31:56 -0500

    enabled cors

--- sample/first-alpaca.py
+++ sample/first-alpaca.py
@@ -2,6 +2,7 @@ import os
 from dotenv import load_dotenv
 from alpaca.trading.client import TradingClient
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 load_dotenv()
 ALPACA_KEY_ID = os.getenv("APCA_API_KEY_ID")
@@ -9,6 +10,18 @@ ALPACA_SECRET_KEY = os.getenv("APCA_API_SECRET_KEY")
 ALPACA_ENDPOINT = os.getenv("APCA_API_ENDPOINT")
 
 app = FastAPI()
+
+origins = [
+    "*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/accounts")
 def read_accounts():
