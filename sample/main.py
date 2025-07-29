06f8ad9df33e587834b9e611c5e107ef04b25f29
dalecb13 <dalecb13@gmail.com>
2025-07-29 16:59:02 -0500

    moved server code to main

---
+++ sample/main.py
@@ -0,0 +1,48 @@
+import os
+from dotenv import load_dotenv
+import requests
+from alpaca.trading.client import TradingClient
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import quiverquant
+
+load_dotenv()
+ALPACA_KEY_ID = os.getenv("APCA_API_KEY_ID")
+ALPACA_SECRET_KEY = os.getenv("APCA_API_SECRET_KEY")
+ALPACA_ENDPOINT = os.getenv("APCA_API_ENDPOINT")
+QUIVER_TOKEN = os.getenv("QUIVERQUANT_AUTH_TOKEN")
+
+quiver = quiverquant.quiver(QUIVER_TOKEN)
+
+app = FastAPI()
+
+origins = [
+    "*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/accounts")
+def read_accounts():
+    trading_client = TradingClient(ALPACA_KEY_ID, ALPACA_SECRET_KEY, paper=True)
+    account = trading_client.get_account()
+    return account
+
+@app.get("/trades")
+def read_trades():
+    headers = {
+        'Authorization': 'Bearer ' + str(QUIVER_TOKEN),
+        'Accept': 'application/json'
+    }
+    params = {
+        'date': '20250728'
+    }
+    trades = requests.get('https://api.quiverquant.com/beta/bulk/congresstrading/', headers=headers, params=params)
+    print(trades)
+    # return trades
