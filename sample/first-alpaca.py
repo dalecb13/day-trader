3dcbec2da59912774a93712d53ee15dda057b670
dalecb13 <dalecb13@gmail.com>
2025-07-29 16:58:52 -0500

    reverted alpaca requests

--- sample/first-alpaca.py
+++ sample/first-alpaca.py
@@ -1,30 +1,20 @@
+import requests
 import os
 from dotenv import load_dotenv
 from alpaca.trading.client import TradingClient
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 
 load_dotenv()
 ALPACA_KEY_ID = os.getenv("APCA_API_KEY_ID")
 ALPACA_SECRET_KEY = os.getenv("APCA_API_SECRET_KEY")
 ALPACA_ENDPOINT = os.getenv("APCA_API_ENDPOINT")
 
-app = FastAPI()
+# ALPACA_ACCOUNT_ENDPOINT = f"{ALPACA_ENDPOINT}/account"
 
-origins = [
-    "*",
-]
+# account_request = requests.get(ALPACA_ACCOUNT_ENDPOINT, auth=(ALPACA_KEY_ID, ALPACA_SECRET_KEY))
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+# print(account_request.json())
 
-@app.get("/accounts")
-def read_accounts():
-    trading_client = TradingClient(ALPACA_KEY_ID, ALPACA_SECRET_KEY, paper=True)
-    account = trading_client.get_account()
-    return account
+trading_client = TradingClient(ALPACA_KEY_ID, ALPACA_SECRET_KEY, paper=True)
+account = trading_client.get_account()
+
+print(account)
